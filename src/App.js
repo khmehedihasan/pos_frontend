cee8556c4112862e4194b5ded4e218479eef791c
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-17 16:19:06 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import AddCategory from './pages/category/AddCategory';
 import Home from './pages/Home';
 
 function App() {
   return (
     <>
-       <Routes>
-         <Route path="/" element={ <Home />} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/addCategory" element={ <AddCategory />} />
+      </Routes>
     </>
   );
 }

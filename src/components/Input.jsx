917d18ecf86efe79d5534da2301379e00e7af09c
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-17 16:23:09 +0600

    input component added

---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Input1({onChange, type, name, value, placeholder, lavel, id}){
+
+    return(
+        <>
+            <label htmlFor={id} className="block cursor-pointer mt-2 mb-1">{lavel}</label>
+            <input className=" w-full outline-none focus:bg-cyan-100 pl-2 py-1 rounded-sm text-gray-600 required:border-red-500" id={id} onChange={onChange} type={type} name={name} value={value} placeholder={placeholder} />
+        </>
+    )
+}
+
+function Textarea({onChange, type, name, value, placeholder, lavel, id}){
+
+    return(
+        <>
+            <label htmlFor={id} className="block cursor-pointer mt-2 mb-1 ">{lavel}</label>
+            <textarea className=" w-full outline-none focus:bg-cyan-100 pl-2 rounded-sm text-gray-600 required:border-red-500" onChange={onChange} id={id} name={name} value={value} placeholder={placeholder} cols="20" rows="10"></textarea>
+        </>
+    )
+}
+
+ export { Input1, Textarea } ;

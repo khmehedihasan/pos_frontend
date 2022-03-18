a052b3ba7bf709f2c5007d77c88ca745482f23af
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-18 12:17:09 +0600

    done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import OutsideClick from '../hooks/OutsideClick';
 import Nav from './Nav';
 import SideBarToggler from './SideBarToggler';
@@ -49,7 +50,8 @@ function SideBar(){
         <>
             
             <div ref={(show === '-ml-80')? null:outClick} className={show + " block ease-in-out duration-700 float-left absolute top-0 left-0 z-10 md:static md:-ml-0 w-1/2 sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-2/12 min-h-screen bg-dark-blue-2"}>
-                <div className=" text-xl text-white font-bold p-6 bg-dark-blue-1"><i className="fa-brands fa-canadian-maple-leaf text-2xl"></i> KH POS</div>
+                
+                <div className=" text-xl text-white font-bold p-6 bg-dark-blue-1"><NavLink to="/"><i className="fa-brands fa-canadian-maple-leaf text-2xl"></i> KH POS</NavLink></div>
                     {
                         data.map((data,index)=>{
                             return(

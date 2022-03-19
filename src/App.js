9ce7307e90b5c5939f13001c7b152fd2cd8a9724
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-19 20:31:09 +0600

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import AddCategory from './pages/category/AddCategory';
 import Home from './pages/Home';
+import useFirstLoad from './hooks/useFirstLoad';
 
 function App() {
+
+  useFirstLoad();
+
   return (
     <>
       <Routes>

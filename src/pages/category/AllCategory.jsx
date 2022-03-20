2e3133d4cda90fa36ca9c3516f89e440dd0905af
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-20 23:49:41 +0600

    show category done

---
+++ src/pages/category/AllCategory.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Delete, Edit, Return, View } from '../../components/Button';
+import Layout from '../../components/Layout';
+import Table, { Tr, Td, Img } from '../../components/Table';
+
+
+function AllCategory(){
+
+    const category = useSelector((state)=>state.category);
+
+    return(
+        <Layout>
+
+            <Table to="/" name="Add Category" rowNames={["#","Name","Description","Poto","Actions"]}>
+                {
+                    category.map(({name, description, img}, index)=>{
+                        return(
+                            <Tr key={index}>
+                                <Td>{index+1}</Td>
+                                <Td>{name}</Td>
+                                <Td>{description}</Td>
+                                <Img>{img}</Img>
+                                <Td>
+                                    <View to="/" />
+                                    <Edit to="/" />
+                                    <Return to="/" />
+                                    <Delete />
+                                </Td>
+                            </Tr>
+                        );
+                    })
+                }
+                
+            </Table>
+
+        </Layout>
+    )
+}
+
+export default AllCategory;

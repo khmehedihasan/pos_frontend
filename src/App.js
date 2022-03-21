2e4d571b26701a7c2a8f9a246f2be9b78350b78c
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-22 01:03:33 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import useFirstLoad from './hooks/useFirstLoad';
 import Home from './pages/Home';
+import Error from './pages/Error';
 import AddCategory from './pages/category/AddCategory';
 import AllCategory from './pages/category/AllCategory';
+import EditCategory from './pages/category/EditCategory';
 
 
 function App() {
@@ -13,8 +15,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={ <Home />} />
-        <Route path="/addCategory" element={ <AddCategory />} />
-        <Route path="/allCategory" element={ <AllCategory />} />
+        <Route path="/category/add" element={ <AddCategory />} />
+        <Route path="/category" element={ <AllCategory />} />
+        <Route path="/category/edit/:id" element={ <EditCategory />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </>
   );

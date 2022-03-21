94ceb12a7c74c6da6a5880d54502b1b2d39c6cce
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-22 01:03:53 +0600

    update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function Input1({onChange, type, name, value, placeholder, lavel, id, fileRef}){
+function Input1({onChange, type, name, value, placeholder, lavel, id }){
 
     return(
         <>
             <label htmlFor={id} className="block cursor-pointer mt-2 mb-1">{lavel}</label>
             <input className=" w-full outline-none focus:bg-cyan-100 pl-2 py-1 rounded-sm text-gray-600 required:border-red-500" id={id} onChange={onChange} type={type} name={name} value={value} placeholder={placeholder} />
+        </>
+    )
+}
+
+function Input2({onChange, type, name, value, placeholder, lavel, id, img}){
+
+    return(
+        <>
+            <label htmlFor={id} className="block cursor-pointer mt-2 mb-1">{lavel}</label>
+            <div className=" w-full flex items-center">
+                <input className=" outline-none focus:bg-cyan-100 pl-2 py-1 rounded-sm text-gray-600 required:border-red-500" id={id} onChange={onChange} type={type} name={name} value={value} placeholder={placeholder} />
+                <img src={img} alt="" width="100" />
+            </div>
+
         </>
     )
 }
@@ -20,4 +34,4 @@ function Textarea({onChange, type, name, value, placeholder, lavel, id}){
     )
 }
 
- export { Input1, Textarea } ;
+ export { Input1, Input2, Textarea } ;

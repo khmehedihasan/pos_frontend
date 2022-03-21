ebc65c1ee3ce264ce8405d21855a7bef9352abb8
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-21 20:46:17 +0600

    dalete category

--- src/store/reducers/category.js
+++ src/store/reducers/category.js
@@ -5,6 +5,14 @@ function category(state = initialState, action){
         case 'ADD_ALL_CATEGORY' : 
             state = action.data;
             return state;
+
+        case 'REMOVE_CATEGORY' : 
+            
+            const ndata = state.filter((data)=>{
+                return(data._id !== action.data);
+            });
+            state = ndata;
+            return state;
            
 
         default : return state;

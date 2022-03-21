2d2ab55f5836a7d80c445fd2b6f2a40942411fa5
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-21 22:27:05 +0600

    update

--- src/store/actions/category.js
+++ src/store/actions/category.js
@@ -3,6 +3,10 @@ export function ADD_ALL_CATEGORY(data){
     return({type:'ADD_ALL_CATEGORY',data});
 }
 
+export function ADD_CATEGORY(data){
+    return({type:'ADD_CATEGORY',data});
+}
+
 export function REMOVE_CATEGORY(data){
     return({type:'REMOVE_CATEGORY',data});
 }

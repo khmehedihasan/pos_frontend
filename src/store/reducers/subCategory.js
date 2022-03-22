2b036648f7ccf614e3e03fc7284d5b3a063e8e53
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-23 00:01:36 +0600

    update

--- src/store/reducers/subCategory.js
+++ src/store/reducers/subCategory.js
@@ -5,7 +5,29 @@ function subCategory(state = initialState, action){
         case 'ADD_ALL_SUB_CATEGORY' : 
             state = action.data;
             return state;
-           
+
+
+        case 'ADD_SUB_CATEGORY' : 
+            
+            state = [...state, action.data]
+            return state;
+
+        case 'REMOVE_SUB_CATEGORY' : 
+            
+            const ndata = state.filter((data)=>{
+                return(data._id !== action.data);
+            });
+            state = ndata;
+            return state;
+
+        case 'UPDATE_SUB_CATEGORY' : 
+            
+            const edata = state.filter((data)=>{
+                return(data._id !== action.data.id);
+            });
+            const pdata = [...edata,action.data.data];
+            state = pdata;
+            return state;
 
         default : return state;
     }

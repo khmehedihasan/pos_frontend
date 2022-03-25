1e8d6db53acc0e1a8e2c46b9ba53e16e25a0bc8c
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-25 21:30:06 +0600

    update

--- src/store/reducers/customer.js
+++ src/store/reducers/customer.js
@@ -5,7 +5,29 @@ function customer(state = initialState, action){
         case 'ADD_ALL_CUSTOMER' : 
             state = action.data;
             return state;
-           
+        
+        case 'ADD_CUSTOMER' : 
+            
+            state = [...state, action.data]
+            return state;
+
+        case 'REMOVE_CUSTOMER' : 
+            
+            const ndata = state.filter((data)=>{
+                return(data._id !== action.data);
+            });
+            console.log(ndata)
+            state = ndata;
+            return state;
+
+        case 'UPDATE_CUSTOMER' : 
+            
+            const edata = state.filter((data)=>{
+                return(data._id !== action.data.id);
+            });
+            const pdata = [...edata,action.data.data];
+            state = pdata;
+            return state;
 
         default : return state;
     }

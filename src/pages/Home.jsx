4aa2017a157e6c0d4c015cfbf6c0e52db1f7856b
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-28 00:01:46 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import Layout from '../components/Layout';
 
 
 function Home(){
+
     return(
         <Layout>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit voluptatum aspernatur laboriosam repellendus asperiores quas suscipit ullam et assumenda, inventore voluptatibus vel iste necessitatibus aliquam quam temporibus eligendi id dicta. </h1>
+            <h1>This is home page.</h1>
         </Layout>
     )
 }

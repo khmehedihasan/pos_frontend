b92b99e5f00d6d016290045c83a477e7d1491f5d
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-31 23:38:57 +0600

    update

--- src/store/reducers/sale.js
+++ src/store/reducers/sale.js
@@ -5,7 +5,11 @@ function sale(state = initialState, action){
         case 'ADD_ALL_SALE' : 
             state = action.data;
             return state;
-           
+
+        case 'ADD_SALE' : 
+            
+            state = [action.data, ...state ]
+            return state;
 
         default : return state;
     }

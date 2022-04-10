881ed18a6cbcf6f525a0d80afc1e865adbf61da3
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-10 17:43:27 +0600

    done

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Alert1, Alert2, AlertContainer } from '../components/Alert';
+
+function LogIn(){
+    const [email, setEmail] = useState('domo@gmail.com');
+    const [password, setPassword] = useState('123456');
+    const [alert, setAlert] = useState([]);
+
+    function send(){
+        if(email === ''){
+            setAlert((alert)=>[...alert, <Alert2 key={ Date.now()} title="Faild!" message='Email is required.' />]);
+        }
+        
+        if( password === ''){
+            setAlert((alert)=>[...alert, <Alert2 key={ Date.now()} title="Faild!" message='password is required.' />]);
+        }
+
+        if(email !=='' & password !== ''){
+            setAlert((alert)=>[...alert, <Alert1 key={ Date.now()} title="Successful" message='Login Successfully.' />]);
+        }
+       
+    }
+    
+    return(
+        <>
+            <div className=" relative w-screen h-screen bg-login bg-center bg-no-repeat bg-cover contrast-150 shadow-2xl flex justify-center items-center">
+                <div className= " absolute top-10 right-2">
+                    <AlertContainer>
+                        {alert}
+                    </AlertContainer>
+                </div>
+
+                <div className=" lgin w-11/12 md:w-3/5 lg:w-2/4 xl:w-1/3 h-96 mx-auto ">
+                    <div >
+                        <h1 className=" text-center text-cyan-300 text-3xl pb-3 border-b border-cyan-400"> Log In</h1>
+                    </div>
+                    <form className=" flex flex-col justify-center p-3 pt-10 gap-y-2" >
+                        <label className=" text-cyan-300" htmlFor="email">Email:</label>
+                        <input onChange={(e)=> setEmail(e.target.value)} className="bg-sky-500/[.4] p-1 rounded-sm text-slate-300 outline-none border-2 border-transparent focus:border-cyan-400" type="email" name="email" value={email} id="email"  />
+                        <label className=" text-cyan-300" htmlFor="password" >Password:</label>
+                        <input onChange={(e)=> setPassword(e.target.value)} className="bg-sky-500/[.4] p-1 rounded-sm text-slate-300 outline-none border-2 border-transparent focus:border-cyan-400"  type="password" name="password" value={password}
+                        id="password" />
+                        <input onClick={send} className=" w-20 p-1 rounded-sm bg-cyan-400 hover:bg-cyan-500" type="button" value="Log In" />
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LogIn;
